73c77cf2e36c7061fa73449e2b1623423948f341
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-13 16:39:00 -0400

    gambling player interface created

--- src/main/java/com/github/zipcodewilmington/casino/games/cardgames/ThreeCardPokerPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/cardgames/ThreeCardPokerPlayer.java
@@ -4,7 +4,7 @@ import com.github.zipcodewilmington.casino.*;
 
 import java.util.List;
 
-public class ThreeCardPokerPlayer extends Player {
+public class ThreeCardPokerPlayer extends Player implements GamblingPlayer{
     private Account account;
     private List<Card> playerHand;
 
@@ -14,5 +14,10 @@ public class ThreeCardPokerPlayer extends Player {
 
     public int playOrFold() {
         return 0; // TODO do we need this method? or should user input call play and fold methods separately
+    }
+
+    @Override
+    public void placeBet(int amount) {
+        // TODO implement betting for 3 card poker
     }
 }
