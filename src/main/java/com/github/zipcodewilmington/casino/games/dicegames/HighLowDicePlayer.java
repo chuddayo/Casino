6cb64402a0582e4582985acbd3574cde06c77ce0
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-13 16:39:12 -0400

    edited HighLowDicePlayer class

--- src/main/java/com/github/zipcodewilmington/casino/games/dicegames/HighLowDicePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/dicegames/HighLowDicePlayer.java
@@ -1,5 +1,7 @@
 package com.github.zipcodewilmington.casino.games.dicegames;
+import com.github.zipcodewilmington.casino.Account;
 
-public class HighLowDicePlayer {
+public class HighLowDicePlayer extends Player implements GamblingPlayer {
 
+    Account account;
 }
