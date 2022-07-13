5673c8a7aff428993d293373efb590ab22284c34
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-13 11:56:04 -0400

    setup enum files and deck constructor

--- src/main/java/com/github/zipcodewilmington/casino/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/Card.java
@@ -37,37 +37,4 @@ public class Card implements Comparable<Card> {
     public int compareTo(Card card) {
         return 0;
     }
-
-    private enum CardValue {
-        TWO(2),
-        THREE(3),
-        FOUR(4),
-        FIVE(5),
-        SIX(6),
-        SEVEN(7),
-        EIGHT(8),
-        NINE(9),
-        TEN(10),
-        JACK(11),
-        QUEEN(12),
-        KING(13),
-        ACE(14);
-
-        private final int cardValue;
-
-        CardValue(int value) {
-            this.cardValue = value;
-        }
-
-        public int getCardValue() {
-            return cardValue;
-        }
-    }
-
-    private enum Suit {
-        CLUBS,
-        DIAMONDS,
-        HEARTS,
-        SPADES;
-    }
 }
--- src/main/java/com/github/zipcodewilmington/casino/Deck.java
+++ src/main/java/com/github/zipcodewilmington/casino/Deck.java
@@ -6,15 +6,29 @@ import java.util.Stack;
 public class Deck {
     Stack<Card> deck;
 
+    public Deck() {
+        this.deck = new Stack<>();
+
+        for (int i = 0; i < 13; i++) {
+            CardValue value = CardValue.values()[i];
+            for (int j = 0; j < 4; j++) {
+                Card card = new Card(value, Suit.values()[j]);
+                this.deck.add(card);
+            }
+        }
+
+        this.shuffle();
+    }
+
     public void shuffle() {
         Collections.shuffle(deck);
     }
 
     public Card dealTop() {
-        return null;
+        return deck.pop();
     }
 
     public void discard(Card card) {
-
+        deck.add(card);
     }
 }
