0890fae2c41ccde3c56531e80055308ea41457fc
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-13 10:41:57 -0400

    skeleton for account

--- src/main/java/com/github/zipcodewilmington/casino/Account.java
+++ src/main/java/com/github/zipcodewilmington/casino/Account.java
@@ -6,4 +6,41 @@ package com.github.zipcodewilmington.casino;
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class Account {
+    private String name;
+    private String password;
+    private double funds;
+
+    public Account(String name, String password, double fund) {
+        this.name = name;
+        this.password = password;
+        this.funds = fund;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getFunds() {
+        return funds;
+    }
+
+    public void setFunds(double funds) {
+        this.funds = funds;
+    }
+
+    public String toString() {
+        return this.name + "," + this.password + "," + String.format("%.2f", this.funds) + "\n";
+    }
 }
