888161450bb1a84b8225c7b677b1725cf4757afc
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-14 17:52:45 -0400

    Resolved some issue in Dice

--- src/main/java/com/github/zipcodewilmington/casino/games/dicegames/HighLowDiceTest.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/dicegames/HighLowDiceTest.java
@@ -6,22 +6,22 @@ import com.github.zipcodewilmington.casino.AccountManager;
 import java.io.IOException;
 import java.util.HashSet;
 
-public class HighLowDiceTest {
-    public static void main(String[] args) throws IOException {
-
-        AccountManager mgr = new AccountManager();
-
-        HashSet<HighLowDicePlayer> players = new HashSet<>();
-
-        HighLowDicePlayer linda = new HighLowDicePlayer(mgr.getAccount("Linda", "password"));
-        HighLowDicePlayer steven = new HighLowDicePlayer(mgr.getAccount("Steven", "password"));
-
-        players.add(linda);
-        players.add(steven);
-
-        HighLowDice game = new HighLowDice(players);
-        game.beginGame();
-
-        mgr.updateAccounts();
-    }
-}
+//public class HighLowDiceTest {
+//    public static void main(String[] args) throws IOException {
+//
+//        AccountManager mgr = new AccountManager();
+//
+//        HashSet<HighLowDicePlayer> players = new HashSet<>();
+//
+//        HighLowDicePlayer linda = new HighLowDicePlayer(mgr.getAccount("Linda", "password"));
+//        HighLowDicePlayer duck = new HighLowDicePlayer(mgr.getAccount("duck", "password"));
+//
+//        players.add(linda);
+//        players.add(duck);
+//
+//        HighLowDice game = new HighLowDice(players);
+//        game.beginGame();
+//
+//        mgr.updateAccounts();
+//    }
+//}
