c15b833edbfeb11afd239ca09979921fb3b04cc6
Tenae <tenae@Zipcoders-MacBook-Pro-107.local>
2022-07-15 17:15:45 -0400

    tic tac game runs

--- src/main/java/com/github/zipcodewilmington/casino/games/TicTacToe/TicTacToePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/TicTacToe/TicTacToePlayer.java
@@ -1,9 +1,28 @@
 package com.github.zipcodewilmington.casino.games.TicTacToe;
 
+import com.github.zipcodewilmington.casino.Account;
+import com.github.zipcodewilmington.casino.Player;
+
 /**
  * Created by leon on 7/21/2020.
  */
-public class TicTacToePlayer {
+public class TicTacToePlayer extends Player {
 
+    String userInput;
+    int attempts;
+    String[][] userInterface;
 
+    public TicTacToePlayer(Account account) {
+        super(account);
+    }
+
+    @Override
+    public String getPlayerName() {
+        return null;
+    }
+
+    @Override
+    public Account getPlayerAccount() {
+        return null;
+    }
 }
