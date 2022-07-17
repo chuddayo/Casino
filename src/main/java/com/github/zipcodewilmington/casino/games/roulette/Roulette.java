2798dc80fed81203417b32330cef18986ac1b026
randy <randyr_1993@yahoo.com>
2022-07-17 14:24:33 -0400

    Committing Roulette class, Roulette Player class and WheelSpin class

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/Roulette.java
@@ -0,0 +1,89 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import com.github.zipcodewilmington.casino.Account;
+import com.github.zipcodewilmington.casino.MultiplayerGamblingGame;
+import com.github.zipcodewilmington.casino.Player;
+import com.github.zipcodewilmington.casino.games.roulette.typeofbet.OddOrEven;
+import com.github.zipcodewilmington.casino.games.roulette.typeofbet.RedOrBlack;
+import com.github.zipcodewilmington.casino.games.roulette.typeofbet.StreetBet;
+import com.github.zipcodewilmington.casino.games.roulette.utilroulette.IOConsoleReader;
+
+import java.util.HashSet;
+
+
+public class Roulette implements MultiplayerGamblingGame {
+
+
+    private final String R = "Roulette";
+    private Bet[] myPossibleBets = {
+            new RedOrBlack("Red or Black", 1),
+            new OddOrEven("Odd or Even", 1),
+            new StreetBet("Three in a Row", 11)
+    };
+
+    private final WheelSpin myWheel;
+
+    public Roulette() {
+        myWheel = new WheelSpin();
+
+    }
+
+    public String getAccount() {
+        return null;
+    }
+
+    public void play(Account account) {
+        int amount = IOConsoleReader.promptRange("How much would you like to bet?", 0, Account.getBalance());
+                                                                                    //will need to get right method
+        Bet bet = promptForBet();
+        bet.place();
+
+        System.out.print("Spinning . . . ");
+        WheelSpin.SpinResult spinResult = myWheel.spin();
+        System.out.println(String.format("Dropped into %s", spinResult));
+        if (bet.isMade(spinResult)) {
+            System.out.println("Congrats! You win!");
+            amount = bet.payout(amount); //I'll have to change this later
+        } else {
+            System.out.println("Sorry, you have lost");
+            amount *= -1;
+        }
+        account.setBalance(amount); //Will have to change this later
+
+
+    }
+
+    private Bet promptForBet() {
+        System.out.println("You can make one of these bets:");
+        for (int k = 0; k < myPossibleBets.length; k++) {
+            System.out.println(String.format("%d) %s", (k + 1), myPossibleBets[k]));
+        }
+
+        int response = IOConsoleReader.promptRange("Please make a choice", 1, myPossibleBets.length);
+        return myPossibleBets[response - 1];
+    }
+
+
+
+
+
+    @Override
+    public void payout(Account account, int payoutAmount) {
+
+    }
+
+    @Override
+    public void beginGame() {
+
+    }
+
+    @Override
+    public String printInstructions() {
+        return null;
+    }
+
+    @Override
+    public HashSet<Player> decideWinner(HashSet<Player> players) {
+        return null;
+    }
+}
---
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
@@ -0,0 +1,28 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import com.github.zipcodewilmington.casino.Account;
+import com.github.zipcodewilmington.casino.GamblingPlayer;
+import com.github.zipcodewilmington.casino.Player;
+
+public class RoulettePlayer extends Player implements GamblingPlayer {
+
+
+    public RoulettePlayer(Account account) {
+        super(account);
+    }
+
+    @Override
+    public void placeBet(int amount) {
+
+    }
+
+    @Override
+    public String getPlayerName() {
+        return null;
+    }
+
+    @Override
+    public Account getPlayerAccount() {
+        return null;
+    }
+}
